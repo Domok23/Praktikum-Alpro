b36f23e7ebb5529a550733431588490427feb0e1
Domok23 <wahyur2bro@gmail.com>
2022-03-22 19:32:36 +0700

    testing

---
+++ Prak 1/Kegiatan/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream.h>
+#include <conio.h>
+
+void main() {
+
+float m, a;
+
+float F;
+
+cout << "Masukkan besarnya massa = ";
+
+cin >> m;
+
+cout << "Masukkan besarnya percepatan = ";
+
+cin >> a;
+
+F = m * a;
+
+cout << "Besarnya gaya = " << F << endl;
+
+}
