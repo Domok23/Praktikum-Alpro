cd13fef83680473757d40555ede9c7dfc3ae9c73
Domok23 <wahyur2bro@gmail.com>
2022-03-23 16:04:52 +0700

    kegiatan prak 1

--- Prak 1/Kegiatan/Penjumlahan.cpp
+++ Prak 1/Kegiatan/Penjumlahan.cpp
@@ -5,12 +5,12 @@ using namespace std;
 class Hitung {
 	friend ostream& operator<<(ostream&, const Hitung&);
 	friend istream& operator>>(istream&, Hitung&);
-public:
-	Hitung();
-	void hitung_jumlahnya(){ jumlah = (a + b + c); }
-private:
-	int a,b,c;
-	int jumlah;
+	public:
+		Hitung();
+		void hitung_jumlahnya(){ jumlah = (a + b + c); }
+	private:
+		int a,b,c;
+		int jumlah;
 };
 
 Hitung::Hitung() {
