9cab9d8b36e1cb4a217a99a05c92981cc854c782
Domok23 <wahyur2bro@gmail.com>
2022-04-07 07:43:34 +0700

    post 3

---
+++ Prak 3/Postest/pangkat.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int x,y,i;
+    int pangkat=1;
+	 
+    cout<<"\t\t\tMENGHITUNG HASIL PERPANGKATAN\n";
+    cout<<endl;
+    cout<<" Masukkan Bilangan = ";
+	cin>>x;
+    cout<<" Masukkan Pangkat  = ";
+	cin>>y;
+    for(i=1;i<=y;i++){
+    	pangkat*=x;
+    }
+    cout<<endl;
+    cout<<" ";
+    cout<<x<<" Pangkat "<<y<<" = "<<pangkat;
+    return 0;
+} 
